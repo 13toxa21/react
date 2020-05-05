1563768c2edc594491b57591eb9ac9631d543f4a
13toxa21 <13toxa21@mail.ru>
2020-05-06 00:20:16 +0300

    Create TodoSearch.jsx

---
+++ src/components/TodoSearch.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TodoSearch = () => {
+    return (
+        <div className="search-panel d-flex">
+        <input className="form-control search-input" type="text" placeholder="Поиск" />
+        <div className="btn-group">
+          <button type="button" className="btn btn-info">Все</button>
+          <button type="button" className="btn btn-outline-secondary">
+            Понравилось
+          </button>
+          <button type="button" className="btn btn-danger">Сброс</button>
+        </div>
+      </div>
+    );
+};
+
+export default TodoSearch;
